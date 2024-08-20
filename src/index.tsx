d8374ae0c374f51dff9c4a1b857402f36e479661
George G <grreulvi@gmail.com>
2024-08-21 01:59:07 +0300

    [module2-task1]

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './pages/main/main.component';
+import App from './components/app/app';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -8,6 +8,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App offersQuantity='5' />
+    <App offersQuantity={5} />
   </React.StrictMode>
 );
